84a366ff3bc93a79f72bcb42043205934ae8848e
rmaes <ikbenruudmaes@gmail.com>
2023-02-07 15:31:25 +0100

    radix baybeeee

--- sources/long.c
+++ sources/long.c
@@ -6,7 +6,7 @@
 /*   By: rmaes <rmaes@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/02/06 17:23:59 by rmaes         #+#    #+#                 */
-/*   Updated: 2023/02/06 17:27:37 by rmaes         ########   odam.nl         */
+/*   Updated: 2023/02/07 15:30:28 by rmaes         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,23 @@
 
 void	sort_long(t_stacks *stacks)
 {
-	ft_printf("insert Radix here pls\n");
-	stacks->a->current = stacks->a->head;
+	unsigned int	i;
+	unsigned int	m;
+
+	m = 1;
+	while (!sorted(stacks))
+	{
+		i = 0;
+		while (i < stacks->a->listlen + stacks->b->listlen)
+		{
+			if ((stacks->a->head->index & m) == m)
+				ra(stacks);
+			else
+				pb(stacks);
+			i++;
+		}
+		while (stacks->b->listlen > 0)
+			pa(stacks);
+		m = m << 1;
+	}
 }
